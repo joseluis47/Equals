37195a559d67a6068407d24680716e223e0afa97
jose luis <jluiss@bol.com.br>
2020-09-06 06:59:41 -0300

    Classes

---
+++ Usuarios/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Usuarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha = @"C:\Users\User\source\repos\Usuarios\usuarios.txt";
+
+        }
+    }
+}
