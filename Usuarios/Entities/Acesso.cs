27dc36d1a43b4352f04d0688ce5d6538a6827d5a
jose luis <jluiss@bol.com.br>
2020-09-06 07:12:44 -0300

    Conclusão

---
+++ Usuarios/Entities/Acesso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Usuarios.Entities
+{
+    class Acesso
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public Acesso(string name, DateTime date)
+        {
+            Name = name;
+            Date = date;
+        }
+
+        public Acesso()
+        {
+        }
+
+
+        public Acesso(string arquivo)
+        {
+
+            string[] linha = arquivo.Split(" ");
+            Name = linha[0];
+            Date = DateTime.Parse(linha[1]);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Acesso))
+            {
+                throw new Exception("Parâmetro não é Acesso");
+            }
+            Acesso outros = obj as Acesso;
+            return Name.Equals(outros.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+    }
+}
--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Usuarios.Entities;
 
 namespace Usuarios
 {
@@ -6,8 +9,31 @@ namespace Usuarios
     {
         static void Main(string[] args)
         {
-            string linha = @"C:\Users\User\source\repos\Usuarios\usuarios.txt";
+            string caminho = @"C:\Users\User\source\repos\Usuarios\usuarios.txt";
+            HashSet<Acesso> acessos = new HashSet<Acesso>();
 
+            try
+            {
+                using(FileStream fs = new FileStream(caminho,FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            Acesso acesso = new Acesso(sr.ReadLine());
+                            acessos.Add(acesso);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            Console.WriteLine($"Tivemos acesso de {acessos.Count} Usuários diferentes");
         }
     }
 }
